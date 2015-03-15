daf8ec5234b8475f1c20fa280828b782a0858922
javacreed <albert@javacreed.com>
2015-03-15 10:53:35 +0100

    Initial Import

---
+++ src/main/java/com/javacreed/examples/app/Main.java
@@ -0,0 +1,33 @@
+/*
+ * #%L
+ * Simple Java Database Swing Application - Part 03 - Connect with Database
+ * %%
+ * Copyright (C) 2012 - 2015 Java Creed
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.javacreed.examples.app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+
+  public static void main(final String[] args) throws SQLException {
+    try (Connection connection = DriverManager.getConnection("jdbc:h2:target/db", "sd", "")) {
+      System.out.println("Connected");
+    }
+  }
+}
